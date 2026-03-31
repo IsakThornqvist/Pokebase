3f796af0fa99793c847876c57bb643309d87cf6d
Isak <it222hp@student.lnu.se>
2026-03-31 16:24:50 +0200

    Initial commit Reacts + Tailwind + TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-200 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
