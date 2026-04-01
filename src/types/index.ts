e0a800d84d100f2279c1c18a1693332518681975
Isak <it222hp@student.lnu.se>
2026-04-01 13:34:12 +0200

    add a temporary interface and dataset to experiment with

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export interface Pokemon {
+  id: string
+  name: string
+  type1: string
+  type2: string | null
+/*   species: string | null
+  height: string | null
+  weight: string | null
+  abilities: string | null */
+  hp: number | null
+  attack: number | null
+  defense: number | null
+  speed: number | null
+/*   catchRate: number | null
+  baseExp: number | null
+  growthRate: string |null
+  eggGroups: string |null
+  gender: string | null */
+
+}
---
+++ src/utils/fakaData.ts
@@ -0,0 +1,9 @@
+import type { Pokemon } from "../types";
+
+export const fakePokemon : Pokemon[] = [
+{ id: "1", name: "Bulbasaur", type1: "Grass", type2: "Poison", hp: 45, attack: 49, defense: 49, speed: 45 },
+{ id: "4", name: "Charmander", type1: "Fire", type2: null, hp: 39, attack: 52, defense: 43, speed: 65 },
+{ id: "7", name: "Squirtle", type1: "Water", type2: null, hp: 44, attack: 48, defense: 65, speed: 43 },
+{ id: "25", name: "Pikachu", type1: "Electric", type2: null, hp: 35, attack: 55, defense: 40, speed: 90 },
+{ id: "6", name: "Charizard", type1: "Fire", type2: "Flying", hp: 78, attack: 84, defense: 78, speed: 100 },
+]
