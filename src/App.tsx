442aa47096363d7c98d67a43b9c1d37417a34cc5
Isak <it222hp@student.lnu.se>
2026-04-01 14:31:16 +0200

    add a basic footer and route imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import Navbar from "./components/Navbar"
-import { BrowserRouter } from 'react-router-dom'
+import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Pokemon from "./pages/Pokemon"
 
 function App() {
   return (
     <BrowserRouter>
     <main className="h-screen">
       <Navbar/>
+      <Routes>
+      <Route path="/pokemon" element={<Pokemon />} />
+      </Routes>
       <h1 className="font-bold text-3xl text-center"> Header</h1>
-
     </main>
+    <Footer/>
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-green-700 px-6 py-10 flex items-center justify-between min-h-16">
+      
+        <p> Footer </p>
+
+    </div>
+  )
+}
+
+export default Footer
