8b5f216df9b204af5c05525221e64836298fa380
Isak <it222hp@student.lnu.se>
2026-04-01 12:12:20 +0200

    Replace a tags with Link from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Navbar from "./components/Navbar"
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <main className="h-screen">
       <Navbar/>
       <h1 className="font-bold text-3xl text-center"> Header</h1>
 
     </main>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   return (
 <nav className="bg-green-700 px-6 py-10 flex items-center justify-between min-h-16">
       <ul className="flex gap-6">
         <li>
-          <a href="/" className="text-white hover:text-blue-400">Home</a>
+          <Link to="/" className="text-white hover:text-blue-400">Home</Link>
         </li>
         <li>
-          <a href="/pagetwo" className="text-white hover:text-blue-400">Page Two</a>
+          <Link to="/pokemon" className="text-white hover:text-blue-400">Pokemon</Link>
         </li>
         <li>
-          <a href="/pagethree" className="text-white hover:text-blue-400">Page Three</a>
+          <Link to="/teams" className="text-white hover:text-blue-400">Teams</Link>
         </li>
       </ul>
     </nav>
