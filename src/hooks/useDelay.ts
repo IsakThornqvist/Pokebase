18eec33796ef80b9e8a88bf46a44fa77c7dadcf8
Isak <it222hp@student.lnu.se>
2026-04-09 17:08:53 +0200

    add documentation for useDelay

--- src/hooks/useDelay.ts
+++ src/hooks/useDelay.ts
@@ -1,3 +1,14 @@
+/**
+ * Returns a value after a specified delay.
+ * Useful for deblaying fast-changing values like search inputs to avoid spammijg the database.
+ *
+ * @param value - The input value.
+ * @param delay - Delay in milliseconds.
+ * @returns The delayed value.
+ * 
+ * @author Isak Thörnqvist
+ * @version 1.0.0
+ */
 import { useEffect, useState } from "react"
 
 export function useDelay<T>(value: T, delay: number) {
