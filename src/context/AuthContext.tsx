2654b43073f41c5bc8367b00ec32ceeafaaef117
Isak <it222hp@student.lnu.se>
2026-04-13 12:56:13 +0200

    add documentation

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,15 +1,33 @@
+/**
+ * Authentication context.
+ *
+ * Provides global auth state and actions.
+ * Persists token in localStorage.
+ *
+ * @author Isak Thörnqvist
+ * @version 1.0.0
+ */
+
 import { createContext, useContext, useState } from "react"
 import type { ReactNode } from "react"
 
+/**
+ * Auth context shape.
+*/
 interface AuthContextType {
     token: string | null
     login: (token: string) => void
     logout: () => void
 }
 
+/**
+ * Auth context instance.
+*/
 const AuthContext = createContext<AuthContextType | null>(null)
 
-
+/**
+ * Provides authentication state to the app.
+*/
 export function AuthProvider({ children }: { children: ReactNode }) {
 
     const [ token, setToken ] = useState<string | null>(
@@ -34,11 +52,14 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     )
 
 }
-/* export function useAuth () {
+/**
+ * Hook for accessing auth context.
+ */
+export function useAuth () {
     const context = useContext(AuthContext)
     if (!context) {
         throw new Error("Error in authcontext.tsx no useAuth")
     }
     return context
 
-} */
+}
