9e795b918c806ed6e37c25bface8d731fd5cb6b3
Isak <it222hp@student.lnu.se>
2026-04-13 12:10:28 +0200

    add optional token argument to graphqlRequest

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -1,13 +1,13 @@
 const API_URL = import.meta.env.VITE_API_URL
 
 // T placeholder used in hooks later
-export async function graphqlRequest<T>(query: string, variables?: Record<string, unknown>,
-): Promise<T> {
+export async function graphqlRequest<T>(query: string, variables?: Record<string, unknown>, token?: string | null): Promise<T> {
 
   const response = await fetch(API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      ...(token && { Authorization: `Bearer ${token}` })
     },
     body: JSON.stringify({ query, variables }),
   });
