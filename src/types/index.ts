1f9a85972241250ef2e92c3f69b2756a7578b4a4
Isak <it222hp@student.lnu.se>
2026-04-17 13:14:38 +0200

    uncomment the Pokemon interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,20 +6,20 @@ export interface Pokemon {
   name: string
   type1: string
   type2: string | null
-/*     species: string | null
-  height: string | null */
+  species: string | null 
+  height: string | null 
   weight: string | null 
-  //abilities: string | null
+  abilities: string | null
   hp: number | null
   attack: number | null
   defense: number | null
   spAttack: number | null
   spDefense: number | null
   speed: number | null
-/*   catchRate: number | null
+  catchRate: number | null
   baseExp: number | null
   growthRate: string |null
   eggGroups: string |null
-  gender: string | null */
+  gender: string | null 
 
 }
